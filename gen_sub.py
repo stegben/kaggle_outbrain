ea4d1563ca0096f95e706c4a2387db326d520327
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-12-18 11:43:59 +0800

    generate submission in local

--- gen_sub.py
+++ gen_sub.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import pandas as pd
@@ -8,7 +9,8 @@ from utils import df2sub
 def main():
     pred_fname = sys.argv[1]
     df_pred = pd.read_csv(pred_fname)
-    df2sub(df_pred).to_csv('agg_' + pred_fname)
+    base_pred_fname = os.path.basename(pred_fname)
+    df2sub(df_pred).to_csv('agg_' + base_pred_fname, header=['display_id', 'ad_id'])
 
 
 if __name__ == '__main__':
