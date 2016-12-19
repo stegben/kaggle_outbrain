3ae0e19dad8ffef2a3a8a2771df97fc908aeb115
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-12-19 11:50:09 +0800

    generate submission in other folder

--- gen_sub.py
+++ gen_sub.py
@@ -10,7 +10,7 @@ def main():
     pred_fname = sys.argv[1]
     df_pred = pd.read_csv(pred_fname)
     base_pred_fname = os.path.basename(pred_fname)
-    df2sub(df_pred).to_csv('agg_' + base_pred_fname, header=['display_id', 'ad_id'])
+    df2sub(df_pred).to_csv('../submission/agg_' + base_pred_fname, header=['ad_id'])
 
 
 if __name__ == '__main__':
