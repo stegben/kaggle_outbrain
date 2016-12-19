18957987c0cb42d1ced7cf512b07335434cf4615
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-12-19 17:19:47 +0800

    add svm file transformer

---
+++ df2libsvm.py
@@ -0,0 +1,55 @@
+import sys
+import pickle as pkl
+
+import numpy as np
+import pandas as pd
+
+
+def df2svm(df, fname, feature_size, is_train=True):
+    df_new = pd.DataFrame()
+    drop_column = ['display_id', 'ad_id']
+    if is_train:
+        drop_column.append('clicked')
+
+    feature_start_idx = 0
+    for idx, col in enumerate(df.drop(drop_column, axis=1)):
+        print(idx, col)
+        df_new[str(idx + 1)] = (df[col].astype(np.int32) + feature_start_idx).astype(str) + ':' + str(1)
+        feature_start_idx += feature_size[col] + 1
+    if is_train:
+        df_new['0'] = df['clicked']
+        drop_column.append('clicked')
+    else:
+        df_new['0'] = 0
+    # with open(fname, 'w') as fw:
+    #     for row in df_new.iterrows():
+    #         fw.write(' '.join([str(r) for r in row]))
+    df_new.sort_index(axis=1).to_csv(fname, sep=' ', header=False, index=False)
+    if is_train:
+        df[['display_id', 'ad_id', 'clicked']].to_csv(fname+'.id', index=False)
+    else:
+        df[['display_id', 'ad_id']].to_csv(fname+'.id', index=False)
+    del df_new
+
+
+def main():
+    print('Read data...')
+    data_fname = sys.argv[1]
+    libsvm_data_fname = sys.argv[2]
+    with open(data_fname, 'rb') as f:
+        data = pkl.load(f)
+    df_subtrain = data['subtrain']
+    df_validation = data['validation']
+    df_test = data['test']
+    feature_size = data['feature_size']
+
+    print('Store svm data')
+    df2svm(df_subtrain, '../subtrain_' + libsvm_data_fname, feature_size, is_train=True)
+    del df_subtrain
+    df2svm(df_validation, '../validation_' + libsvm_data_fname, feature_size, is_train=True)
+    del df_validation
+    df2svm(df_test, '../test_' + libsvm_data_fname, feature_size, is_train=False)
+
+
+if __name__ == '__main__':
+    main()
